f38d34b60dcd1e659a52cb2bd7d3d4028e264ef9
Torben Werner <torben.werner@microsoft.com>
2020-12-14 20:15:18 +0100

    Key Management Azure Function added to repository

---
+++ src/key-management/KeyManagementFunction/Models/KeyUpdateMessage.cs
@@ -0,0 +1,40 @@
+﻿using Newtonsoft.Json;
+
+namespace KeyManagementFunction.Models
+{
+    public class KeyUpdateMessage
+    {
+        [JsonProperty("key")]
+        public KeyUpdateMessagePayload Key { get; set; }
+
+        public KeyUpdateMessage(string deviceId, string shortId, string newKey)
+        {
+            Key = new KeyUpdateMessagePayload
+            {
+                Action = "add",
+                Type = "MIOTY",
+                Uid = deviceId,
+                ShortId = shortId,
+                NwkKey = newKey
+            };
+        }
+    }
+
+    public class KeyUpdateMessagePayload
+    {
+        [JsonProperty("action")]
+        public string Action { get; set; }
+
+        [JsonProperty("type")]
+        public string Type { get; set; }
+
+        [JsonProperty("uid")]
+        public string Uid { get; set; }
+
+        [JsonProperty("shortId")]
+        public string ShortId { get; set; }
+
+        [JsonProperty("nwkKey")]
+        public string NwkKey { get; set; }
+    }
+}
